3b683ffedcfd9f78de64a6ac57a29393b044d26e
Juan Loera <juan.loera@banno.com>
2015-09-28 11:07:32 -0500

    Created style.scss and modified gulfile.js to convert scss to css and watch for changes to reload server and added dependecy gulp-sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,17 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect');
+var sass = require('gulp-sass');
+
+gulp.task('sass',function(){
+  gulp.src('./sass/**/*.{scss,sass}')
+  .pipe(sass().on('error', sass.logError))
+  .pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass:watch', function(){
+  gulp.watch('./sass/**/*.{scss,sass}', ['sass']);
+  gulp.watch('./css/**/*.css', ['html']);
+});
 
 gulp.task('connect', function() {
   connect.server({
@@ -10,12 +22,12 @@ gulp.task('connect', function() {
 });
 
 gulp.task('html', function () {
-  gulp.src('./*.html')
+  gulp.src('./*.{html,css}')
     .pipe(connect.reload());
 });
 
-gulp.task('watch', function () {
+gulp.task('html:watch', function () {
   gulp.watch(['./*.html'], ['html']);
 });
 
-gulp.task('default', ['connect','watch']);
+gulp.task('default', [ 'sass', 'sass:watch', 'connect','html:watch']);
