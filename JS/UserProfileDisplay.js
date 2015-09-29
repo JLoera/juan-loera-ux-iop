aa2fcbd9fab4877cd9c41a6fa4dc11261825faa1
Juan Loera <juan.loera@banno.com>
2015-09-29 08:41:05 -0500

    Added toggle functionality for user profile properties. (needs some styling, waiting for unit 3 merges)

---
+++ JS/UserProfileDisplay.js
@@ -0,0 +1,20 @@
+function toggleUserProperties(){
+  if(document.getElementById("UserPhone").style.display != "none"){
+    hideUserProperties();
+  }
+  else{
+    showUserProperties();
+  }
+}
+
+function hideUserProperties(){
+  document.getElementById("PropertiesToggle").innerHTML = "+";
+  document.getElementById("UserPhone").style.display = "none";
+  document.getElementById("UserEmail").style.display = "none";
+}
+
+function showUserProperties(){
+  document.getElementById("PropertiesToggle").innerHTML = "-";
+  document.getElementById("UserPhone").style.display = "block";
+  document.getElementById("UserEmail").style.display = "block";
+}
