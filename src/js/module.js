3c92017b29269a7c81e78bc92bee37c5e9f1ec9b
Juan Loera <juan.loera@banno.com>
2015-10-08 13:33:44 -0500

    Errors with variable myApp and jshint

--- src/js/module.js
+++ src/js/module.js
@@ -1,35 +1,3 @@
 'use strict';
 
 var myApp = angular.module('myApp', ['ui.router','ngResource']);
-
-myApp.config(function($stateProvider, $urlRouterProvider) {
-  //
-  // For any unmatched url, redirect to /state1
-  $urlRouterProvider.otherwise('list');
-  //
-  // Now set up the states
-  $stateProvider
-	.state('list', {
-		url: '/list',
-		template: '<user-list></user-list>',
-		controller: 'ListController'
-	})
-
-  .state('profile', {
-		url: '/profile/:userId',
-		template: '<user-profile></user-profile>',
-		controller: 'ProfileController'
-	})
-
-	.state('edit', {
-		url: '/edit/:userId',
-		template: '<user-edit></user-edit>',
-		controller: 'UpdateController'
-	})
-
-	.state('new', {
-		url: '/new',
-		template: '<user-new></user-new>',
-		controller: 'CreateController'
-	});
-});
